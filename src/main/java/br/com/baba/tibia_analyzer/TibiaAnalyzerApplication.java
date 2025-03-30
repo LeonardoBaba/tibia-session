a07ceb82d85b520d0ac2bf0c43624c8db3598c25
lbaba <leobaba111@gmail.com>
2025-03-30 12:10:27 -0300

    feat: initial project setup with dependencies

---
+++ src/main/java/br/com/baba/tibia_analyzer/TibiaAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package br.com.baba.tibia_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TibiaAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TibiaAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/baba/tibia_analyzer/TibiaAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.baba.tibia_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibiaAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
